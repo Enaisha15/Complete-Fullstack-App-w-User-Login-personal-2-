28ecb2de227b7e4a5767632b7e91f00889e3425f
Enaisha <Enaisha15@gmail.com>
2021-10-24 18:09:36 -0400

    enaisha fullstackapp2

---
+++ config/database.js
@@ -0,0 +1,19 @@
+const username  = 'Enaisha15'
+const password  = 'Jasmine123!'
+const dbName    = 'budget-tracker'
+
+module.exports = {
+    'url' : `mongodb+srv://${username}:${password}@cluster0.bgwen.mongodb.net/${dbName}?retryWrites=true&w=majority`
+};
+
+
+
+// const username  = 'Enaisha15'
+// const password  = 'Jasmine123!'
+// const dbName    = 'budget-tracker'
+
+// module.exports = {
+//     'url' : `mongodb+srv://${username}:${password}@cluster0.jrgra.mongodb.net/${dbName}?retryWrites=true&w=majority`
+//     // mongodb+srv://Enaisha15:<password>@cluster0.jrgra.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+// };
+
